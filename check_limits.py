3e88fad89c07d65e68f2eeb6026b5430e058c251
PREETI <preetikadyan634@gmail.com>
2021-03-03 13:01:41 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -5,7 +5,7 @@ limit = {
         } 
  
 def collect_out_of_range_battery_parameters(parameters_exceeded_limit,parameter_name,parameter_value,parameter_limit):
-     if parameter_value < parameter_limit['min'] or parameter_value < parameter_limit['max']:
+     if parameter_value < parameter_limit['min'] or parameter_value > parameter_limit['max']:
           parameters_exceeded_limit.append(parameter_name)
             
 def report_out_of_limit_battery_parameters(Battery_Life_Parameters):

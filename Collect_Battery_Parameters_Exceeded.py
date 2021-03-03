c115823b3306d02b8a6985f529a73065c0c1a4a0
PREETI <preetikadyan634@gmail.com>
2021-03-03 11:29:26 +0530

    Delete Collect_Battery_Parameters_Exceeded.py

--- Collect_Battery_Parameters_Exceeded.py
+++
@@ -1,5 +0,0 @@
-from check_limits import *
-
-def collect_out_of_range_battery_parameters(parameters_exceeded_limit,parameter_name,parameter_value,parameter_limit):
-     if parameter_value < parameter_limit['min'] or parameter_value > parameter_limit['max']:
-        parameters_exceeded_limit.append(parameter_name)

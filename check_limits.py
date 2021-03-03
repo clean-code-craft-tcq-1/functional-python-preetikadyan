c51446905bcb5e99dcd8e1c3554fddd045836c2b
PREETI <preetikadyan634@gmail.com>
2021-03-03 12:43:36 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -13,21 +13,19 @@ def report_out_of_limit_battery_parameters(Battery_Life_Parameters):
      parameters_exceeded_limit = []
      for battery_parameter in Battery_Life_Parameters:
          collect_out_of_range_battery_parameters(parameters_exceeded_limit,battery_parameter,Battery_Life_Parameters[battery_parameter],limit[battery_parameter])
-     return parameters_exceeded_limit    
-     
+     return parameters_exceeded_limit 
+
+ def raise_alert_battery_if_parameters_exceeded_limit(Battery_Life_Parameters):
+     battery_parameters_exceeded = report_out_of_limit_battery_parameters(Battery_Life_Parameters)
+
+     if len(battery_parameters_exceeded) >= 1 :
+        print(len(battery_parameters_exceeded),"battery Parameters exceeded limit")
+     else :
+        print("No battery parameter exceeded limit")     
     
 
 if __name__ == '__main__':
-    
-  Battery_Life_Parameters = {
-       'temperature' : 25,
-       'state_of_charge' : 70,
-       'charge_rate' : 0.7
-          }   
+      
+raise_alert_battery_if_parameters_exceeded_limit({'temperature' : 25, 'state_of_charge' : 70, 'charge_rate' : 0.7})
+raise_alert_battery_if_parameters_exceeded_limit({'temperature' : 50, 'state_of_charge' : 85, 'charge_rate' : 0})
 
-battery_parameters_exceeded = report_out_of_limit_battery_parameters(Battery_Life_Parameters)
-
-if len(battery_parameters_exceeded) >= 2 :
-     print("High Breach")
-else :
-     print("Low Breach")

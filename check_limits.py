55de469be785c06d1a02e329b6ef239518d72768
PREETI <preetikadyan634@gmail.com>
2021-03-03 11:44:33 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,27 +1,29 @@
 limit = {
      'temperature': {'min': 0, 'max': 45},
-     'state_of_charge': {'min': 20, 'max': 80},
-     'charge_rate': {'min': 0,'max': 0.8}
-        }
-
+     'state_of_change': {'min': 20, 'max': 80},
+     'charge_rate': {'max': 0.8}
+        } 
+ 
 def collect_out_of_range_battery_parameters(parameters_exceeded_limit,parameter_name,parameter_value,parameter_limit):
-     if parameter_value < parameter_limit['min'] or parameter_value > parameter_limit['max']:
-        parameters_exceeded_limit.append(parameter_name)
-         
+     if parameter_value < parameter_limit['min'] or parameter_value < parameter_limit['max']:
+         parameters_exceeded_limit.append(parameter_name)
+       
+     
 def report_out_of_limit_battery_parameters(Battery_Life_Parameters):
      parameters_exceeded_limit = []
-     for battery_parameter in Battery_Life_Parameters:
-         collect_out_of_range_battery_parameters(parameters_exceeded_limit,battery_parameter,Battery_Life_Parameters[battery_parameter],limit[battery_parameter])
+     for battery_paramater in Battery_Life_Parameters:
+         collect_out_of_range_battery_parameters(parameters_exceeded_limit,battery_paramater,Battery_Life_Parameters[battery_paramater],limit[battery_parameter])
      return parameters_exceeded_limit    
      
+    
 
 if __name__ == '__main__':
-     
-Battery_Life_Parameters = {
+    
+  Battery_Life_Parameters = {
        'temperature' : 25,
        'state_of_charge' : 70,
        'charge_rate' : 0.7
-          }  
+          }   
 
 battery_parameters_exceeded = report_out_of_limit_battery_parameters(Battery_Life_Parameters)
 

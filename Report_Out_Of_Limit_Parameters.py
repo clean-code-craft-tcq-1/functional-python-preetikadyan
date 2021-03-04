472afef7aaefd7f897cfe83d54ac186dda96ec36
PREETI <preetikadyan634@gmail.com>
2021-03-04 11:53:48 +0530

    Update Report_Out_Of_Limit_Parameters.py

--- Report_Out_Of_Limit_Parameters.py
+++ Report_Out_Of_Limit_Parameters.py
@@ -1,7 +1,7 @@
 from Collect_Out_Of_Limit_Parameters import *
 
 def report_out_of_limit_battery_parameters(Battery_Life_Parameters):
-     parameters_exceeded_limit = {}
+     parameters_exceeded_limit = []
      for battery_parameter in Battery_Life_Parameters:
           collect_out_of_range_battery_parameters(parameters_exceeded_limit,battery_parameter,Battery_Life_Parameters[battery_parameter],limit[battery_parameter])
      return parameters_exceeded_limit 

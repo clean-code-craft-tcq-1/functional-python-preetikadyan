c4b581ddaeffd534ca3b7ddbd50e7f277fb435c0
PREETI <preetikadyan634@gmail.com>
2021-03-04 11:53:03 +0530

    Update Collect_Out_Of_Limit_Parameters.py

--- Collect_Out_Of_Limit_Parameters.py
+++ Collect_Out_Of_Limit_Parameters.py
@@ -1,7 +1,6 @@
 from check_limits import *
 
 def collect_out_of_range_battery_parameters(parameters_exceeded_limit,parameter_name,parameter_value,parameter_limit):
-     if parameter_value < parameter_limit['min']:
-          parameters_exceeded_limit.append(parameter_name,"Under Limit")
-     if parameter_value > parameter_limit['max']:
-          parameters_exceeded_limit.append(parameter_name,"Over Limit")
+     if parameter_value < parameter_limit['min'] or parameter_value > parameter_limit['max']:
+          parameters_exceeded_limit.append(parameter_name)
+     

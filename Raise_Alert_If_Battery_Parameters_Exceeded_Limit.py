9e128ee2eba0950daec9e4fff91a4dc48dd9ece1
PREETI <preetikadyan634@gmail.com>
2021-03-04 12:02:28 +0530

    Update Raise_Alert_If_Battery_Parameters_Exceeded_Limit.py

--- Raise_Alert_If_Battery_Parameters_Exceeded_Limit.py
+++ Raise_Alert_If_Battery_Parameters_Exceeded_Limit.py
@@ -1,9 +1,11 @@
 from Report_Out_Of_Limit_Parameters import *
 from Collect_Out_Of_Limit_Parameters import *
 
-def raise_alert_battery_if_parameters_exceeded_limit(Battery_Life_Parameters):
+def raise_alert_battery_if_parameters_not_within_limit(Battery_Life_Parameters):
      battery_parameters_exceeded = report_out_of_limit_battery_parameters(Battery_Life_Parameters)
-     if len(battery_parameters_exceeded) >= 1 :
-          print(len(battery_parameters_exceeded),"battery Parameters exceeded limit")
+     if len(battery_parameters_exceeded) > 1 :
+          print(len(battery_parameters_exceeded),"battery Parameters not within limit--->High Breach")
+     elif len(battery_parameters_exceeded) == 1:
+          print(len(battery_parameters_exceeded),"battery Parameters not within limit--->Low Breach")
      else :
           print("No battery parameter exceeded limit") 
